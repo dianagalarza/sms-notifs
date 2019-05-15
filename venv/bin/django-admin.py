0fd0201c1eac2f5a2e2432c8c96a572fcc4826bc
dianagalarza <dgalarza01@gmail.com>
2019-05-14 21:28:16 -0400

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dianagalarza/Downloads/server-notifications-django-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
